71cb93f7839eaa3fe79c1c32c3add088cc3fe327
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-03-25 19:03:10 +0100

    Create com.js

---
+++ com.js
@@ -0,0 +1,21 @@
+(function(){
+	var keyss=true;  
+function compi(){
+	 keyss=false;
+        var values=$("textarea").val();
+        var converter = new showdown.Converter();
+        var valuesss=converter.makeHtml(values);
+	
+        $("div.htmlhtml").html(valuesss);
+        $("code.htmltext").text(valuesss);
+      }
+	  $("textarea").val("");
+	 
+    $.get("demo.md", function(data, status){
+	    if(keyss){
+	    $("textarea").val(data);}
+    }, "text");
+    $(document).ready(function(){
+      $("textarea").keyup(compi);
+    });
+  })();
